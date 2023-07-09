442a976d202cee991d0acc0de6afd0f09a515ce7
Felipe <felipe.duarte@ifc.edu.br>
2023-07-09 19:01:38 -0300

    Update exemp1.py

--- code/exemplos/lacos/exemp1.py
+++ code/exemplos/lacos/exemp1.py
@@ -3,7 +3,7 @@ cont = 1
 while True:
   valor = int(input('Digite um valor inteiro: '))
   print("Inteiro: %d, Contador:%d" % (valor, cont))
-  if valor < 0: pass
-  elif valor > 0: continue
+  if valor < 0: pass         # cont só recebe acréscimo em valor < 0
+  elif valor > 0: continue   # se valor > 0, cont não recebe acréscimo
   else: break                # iteração quebra quando valor = 0
-  cont = cont + 1            # cont só recebe acréscimo em valor < 0
+  cont = cont + 1            
